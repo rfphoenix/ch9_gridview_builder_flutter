0740797dc36dc6c754dbd2d10b4fe4726d4a7dc2
Johnson Wang <freelance@rfphoenix.com>
2020-08-28 00:32:14 -0700

    Initial Commit

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:ch9_gridview_builder_flutter/pages/home.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Grid View Builder Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: Home(title: 'Grid View Builder Demo'),
+    );
+  }
+}
